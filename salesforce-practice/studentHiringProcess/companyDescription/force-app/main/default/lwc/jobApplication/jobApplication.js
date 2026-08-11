8d19a2078e625490118daa4d9855577becf86108
NELLURI_KOUSHIK <koushiknelluri827@gmail.com>
2026-08-11 11:38:42 +0530

    add : LDS/Wire strategy, validation approach, debugging issue, and architectural decisions

--- salesforce-practice/studentHiringProcess/companyDescription/force-app/main/default/lwc/jobApplication/jobApplication.js
+++ salesforce-practice/studentHiringProcess/companyDescription/force-app/main/default/lwc/jobApplication/jobApplication.js
@@ -5,16 +5,27 @@ import { NavigationMixin } from 'lightning/navigation';
 import ROLE_NAME_FIELD
     from '@salesforce/schema/Hiring_Company__c.Role_Name__c';
 
+import COMPANY_NAME_FIELD
+    from '@salesforce/schema/Hiring_Company__c.Name';
+
+import ROLE_DESCRIPTION_FIELD
+    from '@salesforce/schema/Hiring_Company__c.Role_Description__c';
+
 export default class JobApplication extends NavigationMixin(LightningElement) {
 
     @api recordId;
 
     company;
     error;
+    showMessage = false;
 
     @wire(getRecord, {
         recordId: '$recordId',
-        fields: [ROLE_NAME_FIELD]
+        fields: [
+            ROLE_NAME_FIELD,
+            COMPANY_NAME_FIELD,
+            ROLE_DESCRIPTION_FIELD
+        ]
     })
     wiredCompany({ data, error }) {
 
@@ -23,30 +34,50 @@ export default class JobApplication extends NavigationMixin(LightningElement) {
             this.error = undefined;
 
             console.log('Hiring Company ID:', this.recordId);
+
+            console.log(
+                'Role Name:',
+                getFieldValue(data, ROLE_NAME_FIELD)
+            );
+
+            console.log(
+                'Company Name:',
+                getFieldValue(data, COMPANY_NAME_FIELD)
+            );
+
+            console.log(
+                'Description:',
+                getFieldValue(data, ROLE_DESCRIPTION_FIELD)
+            );
         }
 
         if (error) {
             this.company = undefined;
             this.error = error;
 
-            console.error('Error:', error);
+            console.error('Error loading Hiring Company:', error);
         }
     }
 
     get roleName() {
+        return this.company
+            ? getFieldValue(this.company, ROLE_NAME_FIELD)
+            : '';
+    }
 
-        if (!this.company) {
-            return '';
-        }
+    get companyName() {
+        return this.company
+            ? getFieldValue(this.company, COMPANY_NAME_FIELD)
+            : '';
+    }
 
-        return getFieldValue(
-            this.company,
-            ROLE_NAME_FIELD
-        );
+    get roleDescription() {
+        return this.company
+            ? getFieldValue(this.company, ROLE_DESCRIPTION_FIELD)
+            : '';
     }
 
     handleApply() {
-
         this[NavigationMixin.Navigate]({
             type: 'standard__objectPage',
             attributes: {
@@ -54,6 +85,9 @@ export default class JobApplication extends NavigationMixin(LightningElement) {
                 actionName: 'new'
             }
         });
+    }
 
+    handleView() {
+        this.showMessage = !this.showMessage;
     }
 }
